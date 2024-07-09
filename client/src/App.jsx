464e9f805d6e4d1a6df6859680eed23323646293
ckiroshan <ckiroshan@gmail.com>
2024-07-09 14:47:24 +0530

    feat: add welcome message before authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
 import logo from "./assets/react.svg";
+import AppContent from "./components/AppContent";
 
 function App() {
   return (
     <>
       <Header pageTitle="Frontend authenticated with JWT" logoSrc={logo} />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col"></div>
+          <AppContent />
+        </div>
+      </div>
     </>
   );
 }
