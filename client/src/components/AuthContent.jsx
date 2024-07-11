30d35855b033e0aea685bc1f66c5aa0f3800b9f0
ckiroshan <ckiroshan@gmail.com>
2024-07-12 01:18:40 +0530

    style: add Bootstrap card layout to AuthContent component

--- client/src/components/AuthContent.jsx
+++ client/src/components/AuthContent.jsx
@@ -12,8 +12,16 @@ const AuthContent = () => {
   }, []);
 
   return (
-    <div>
-      <ul>{data.length > 0 && data.map((line) => <li key={line.id || line}>{line}</li>)}</ul>
+    <div className="row justify-content-md-center">
+      <div className="col-4">
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Backend Response</h5>
+            <p className="card-text">Content:</p>
+            <ul>{data.length > 0 && data.map((line) => <li key={line.id || line}>{line}</li>)}</ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
